0c336a2f2995f6766cf8d83777792b078ebc86f0
Rajveer Kajle <101015360+Rajveer188@users.noreply.github.com>
2025-02-12 15:44:21 +0530

    Delete warehouse_management_system directory

--- warehouse_management_system/Electronics.java
+++
@@ -1,9 +0,0 @@
-package com.tit.week04.day01.generics.warehouse_managment_system;
-
-//class to represent electronics item
-public class Electronics extends WarehouseItem {
-    //constructor
-    public Electronics(String itemName, int itemId) {
-        super(itemName, itemId);
-    }
-}
