3ecda558238df8de04f1ade84be08602114f46bb
Rajveer188 <veerkajle188@gmail.com>
2025-02-16 21:45:55 +0530

    [Rajveer Kajle] Add. java code to Invoke Private Method using reflection

---
+++ src/main/java/com/tit/week04/day06/reflection/basic_level/invoke_private_method/Calculator.java
@@ -0,0 +1,10 @@
+package com.tit.week04.day06.reflection.basic_level.invoke_private_method;
+
+public class Calculator {
+    public Calculator() {}
+
+    //private method
+    private int multiply(int a, int b){
+        return a*b;
+    }
+}
