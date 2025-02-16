d63ae11f192eb46b0a2d153de65a6c9371f3a81b
Rajveer188 <veerkajle188@gmail.com>
2025-02-16 23:08:07 +0530

    [Rajveer Kajle] Add. java code to Retrieve Annotations at Runtime using reflection

---
+++ src/main/java/com/tit/week04/day06/reflection/intermediate_level/retrieve_annotaions/Book.java
@@ -0,0 +1,13 @@
+package com.tit.week04.day06.reflection.intermediate_level.retrieve_annotaions;
+
+//add annotation to class
+@Author(name = "Rajveer")
+public class Book{
+    //attribute
+    private String title;
+
+    //constructor
+    public Book(String title) {
+        this.title = title;
+    }
+}
