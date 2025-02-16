cf44d39df0fd16f7432a7cab2fc1474bc58feb7a
Rajveer188 <veerkajle188@gmail.com>
2025-02-16 21:45:31 +0530

    [Rajveer Kajle] Add. java code to Access Private Field using reflection

---
+++ src/main/java/com/tit/week04/day06/reflection/basic_level/access_private_field/Person.java
@@ -0,0 +1,10 @@
+package com.tit.week04.day06.reflection.basic_level.access_private_field;
+
+public class Person {
+    //private field
+    private int age;
+    //constructor
+    public Person(int age){
+        this.age = age;
+    }
+}
