38324699e5cead95524c9a20c46936ee3e316094
Rajveer188 <veerkajle188@gmail.com>
2025-02-16 23:15:00 +0530

    [Rajveer Kajle] Add. java code to Create a Custom Annotation and Use It

---
+++ src/main/java/com/tit/week04/day06/annotations/exercise/custom_annotation/TaskInfo.java
@@ -0,0 +1,10 @@
+package com.tit.week04.day06.annotations.exercise.custom_annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TaskInfo{
+    int priority() default 1;
+    String assignTo() default  "unassigned";
+}
